83b1cbb13b51cd48c5ab7b131fdeab07f20944c2
Susmita Debnath <supti.debnath@gmail.com>
2017-09-01 22:06:06 +0600

    cin in cpp for receiving user.

---
+++ cin-in-cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    float avg;
+
+    cout << "Enter your age and average"<<endl;
+
+    cin >> age >> avg;
+
+    cout << "you have entered "<< age <<endl << "average is "<< avg;
+
+    return 0;
+}
