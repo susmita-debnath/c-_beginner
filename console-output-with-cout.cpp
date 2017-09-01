eccc5e18b44af9543ca82f7a5bc4aefb0ca68542
Susmita Debnath <supti.debnath@gmail.com>
2017-09-01 21:51:17 +0600

    creating object.

---
+++ console-output-with-cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 23;
+    float avg = 32.53;
+
+    cout << "hi anil " << age << endl << avg;
+
+    return 0;
+}
---
+++ create-object.cpp
@@ -0,0 +1,21 @@
+/// Create object
+
+#include<iostream>
+using namespace std;
+
+class rectangle {
+    public:
+        int height;
+        int width;
+};
+
+int main()
+{
+    rectangle obj;
+    obj.height = 5;
+    obj.width = 6;
+
+    cout<<"Area = "<<obj.height*obj.width<<endl;
+
+    return 0;
+}
