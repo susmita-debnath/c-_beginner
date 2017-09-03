62b1491c91436f94ec23ca0ededb594a1baa2ede
Susmita Debnath <supti.debnath@gmail.com>
2017-09-03 16:47:14 +0600

    member function.

---
+++ member-function.cpp
@@ -0,0 +1,26 @@
+/// member function
+
+#include<iostream>
+using namespace std;
+
+class rectangle {
+    public:
+        int height;
+        int width;
+        int area();
+};
+int rectangle::area()
+{
+    return height*width;
+}
+
+int main()
+{
+    rectangle obj;
+    obj.height = 5;
+    obj.width = 6;
+
+    cout<<"Area = "<<obj.area()<<endl;
+
+    return 0;
+}
