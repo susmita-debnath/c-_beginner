7f76f3aa0a7e28641c889fa928fe90581b8e995a
Susmita Debnath <supti.debnath@gmail.com>
2018-02-14 20:34:22 +0600

    understanding a simple cpp program.

---
+++ nested_if_else_and_if_elseif.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 50;
+
+    if( age <= 20 ){
+            if(){
+                if(){
+
+                }
+            }else{
+
+            }
+
+        cout << "you are young";
+    }else if ( age <= 40){
+
+        cout << "you are in middle age";
+
+    }else{
+            cout << "you are a old guy";
+    }
+
+    return 0;
+}
