34b71b8c4eaf454e8e35763c37f1afc9f4447c87
Susmita Debnath <supti.debnath@gmail.com>
2018-04-12 20:58:35 +0600

    object pointer in cpp.

--- object_pointer.cpp
+++ object_pointer.cpp
@@ -12,12 +12,26 @@ public:
 
 int main()
 {
-    rectangle obj;
-    rectangle *p;
+    //rectangle obj;
+      rectangle *p;
+//    obj.set(3,4);
+//    cout << obj.area() << endl;
+//    p = &obj;
+//
+//    p->set(2,7);
+//    cout << obj.area() << endl;
 //    rectangle obj[5];
-//    obj[0].set(1,2);
-//    obj[1].set(2,2);
-//    obj[2].set(3,2);
-//    obj[3].set(4,2);
-//    obj[4].set(5,2);
+    rectangle obj[5];
+    obj[0].set(1,2);
+    obj[1].set(2,2);
+    obj[2].set(3,2);
+    obj[3].set(4,2);
+    obj[4].set(5,2);
+
+    p = obj;
+
+    for ( int i = 0; i < 5; i++ )
+      cout << i << " = " << (p+i)->area() << endl;
+
+    return 0;
 }
